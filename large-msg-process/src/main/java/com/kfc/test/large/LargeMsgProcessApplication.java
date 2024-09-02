6448f4c6051b6fc98e732bc32c6080eca007da29
chenkf <578956926@qq.com>
2024-09-02 10:51:25 +0800

    first commit

---
+++ large-msg-process/src/main/java/com/kfc/test/large/LargeMsgProcessApplication.java
@@ -0,0 +1,13 @@
+package com.kfc.test.large;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeMsgProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LargeMsgProcessApplication.class, args);
+	}
+
+}
